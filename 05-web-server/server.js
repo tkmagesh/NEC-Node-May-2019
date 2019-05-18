c356a2118795d4967ee89e5214690a70ed651726
Magesh <tkmagesh77@gmail.com>
2019-05-18 15:42:54 +0530

    web server

---
+++ 05-web-server/server.js
@@ -0,0 +1,32 @@
+var http = require('http'),
+	path = require('path'),
+	url = require('url'),
+	fs = require('fs');
+
+var server = http.createServer(function(req /* IncomingMessage (ReadableStream) */, res /* ServerResponse (WritableStream) */){
+	var urlObj = url.parse(req.url);
+
+	var resource = path.join(__dirname, urlObj.pathname);
+	console.log(req.method + '\t' + urlObj.pathname);
+	if (!fs.existsSync(resource)){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+
+	var readStream = fs.createReadStream(resource);
+
+	readStream.pipe(res);
+
+	readStream.on('error', function(err){
+		res.statusCode = 500;
+		res.end();
+	});
+	
+});
+
+server.listen(8080);
+
+server.on('listening', function(){
+	console.log('server listening on 8080');
+});
