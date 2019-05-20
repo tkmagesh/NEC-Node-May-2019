2a8d0ce5d2c265e87ff6fbcde0f92654e0b2dba0
Magesh <tkmagesh77@gmail.com>
2019-05-20 15:37:57 +0530

    taskRoutes updated

--- 10-task-manager/routes/tasks.js
+++ 10-task-manager/routes/tasks.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	uuid = require('uuid/v4');
 
 var taskList = [
 	
@@ -10,7 +11,7 @@ router.get('/', function(req, res, next){
 });
 
 router.get('/:id', function(req, res, next){
-	var taskId = parseInt(req.params.id);
+	var taskId = req.params.id;
 	var result = taskList.find(function(task){
 		return task.id === taskId;
 	});
@@ -27,6 +28,43 @@ router.post('/', function(req, res, next){
 	//generate new id and assign if id === 0
 	//add the item to the array
 	//return 201 status with the newly created task data
-})
+	var newTaskData = req.body;
+	if (newTaskData.id === 0)
+		newTaskData.id = uuid();
+	taskList.push(newTaskData);
+	res.status(201).json(newTaskData);
+});
+
+router.put('/:id', function(req, res, next){
+	var taskId = req.params.id,
+		updatedTask = req.body;
+
+	var result = taskList.find(function(task){
+		return task.id === taskId;
+	});
+	if (!result){
+		res.status(404).end();
+	} else {
+		taskList = taskList.map(function(task){
+			return task.id === taskId ? updatedTask : task;
+		});
+		res.status(200).json(updatedTask);
+	}
+});
+
+router.delete('/:id', function(req, res, next){
+	var taskId = req.params.id;
+	var result = taskList.find(function(task){
+		return task.id === taskId;
+	});
+	if (!result){
+		res.status(404).end();
+	} else {
+		taskList = taskList.filter(function(task){
+			return task.id !== taskId
+		});
+		res.status(200).end();
+	}
+});
 
 module.exports = router;
