a290814c0dd3bde27acd239902ce74028683a7ca
Magesh <tkmagesh77@gmail.com>
2019-05-20 14:16:17 +0530

    npm demo

--- 08-web-app-server-refactored-04/logger.js
+++ 08-web-app-server-refactored-04/logger.js
@@ -1,12 +1,14 @@
+var chalk = require('chalk');
+
 module.exports = function(req, res, next){
 	var startTime = new Date();
-	var logMessage = req.method + '\t' + req.urlObj.pathname;
+	var logMessage = chalk.red(req.method) + '\t' + chalk.blue(req.urlObj.pathname);
 	
 	res.on('finish', function(){
-		logMessage += '\t' + res.statusCode;
+		logMessage += '\t' + chalk.green(res.statusCode);
 		var endTime = new Date(),
 			elapsed = endTime - startTime;
-		logMessage += '\t' + elapsed + 'ms';
+		logMessage += '\t' + chalk.magenta(elapsed) + 'ms';
 		console.log(logMessage);
 	});
 	next();
